ac326e2c2fcb34673cd544782602eae99a8725f1
jpenaunilibre <jandy.pena.ttc.506@unilibre.edu.co>
2024-11-14 20:04:33 -0500

    .

--- script1.js
+++ script1.js
@@ -1,14 +1,13 @@
 let numeros = [5,1,8,6,7,8,6,9];
-let sumatoria=0;
+let sumatoria = 0;
 let promedio = 0;
 
-for (i=0; i<numeros.length; i++){
-    sumatoria=+numeros[i];    
+for (let i = 0; i < numeros.length; i++) {
+    sumatoria += numeros[i];    
 }
 
-promedio = sumatoria/numeros.length;
+promedio = sumatoria / numeros.length;
+
+console.log(sumatoria); // Esto debería mostrar el total correcto de la suma
 document.getElementById("resultado").value = sumatoria;
-
 document.getElementById("promedio").value = promedio;
-
-console.log(sumatoria);
