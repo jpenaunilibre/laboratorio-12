7c8ed5f3b81c778950e94344b1769f174dcd4415
jpenaunilibre <jandy.pena.ttc.506@unilibre.edu.co>
2024-11-14 19:48:37 -0500

    agregué el promedio

--- script1.js
+++ script1.js
@@ -1,8 +1,12 @@
 let numeros = [5,1,8,6,7,8,6,9];
 let sumatoria=0;
+let promedio = 0;
 
 for (i=0; i<numeros.length; i++){
     sumatoria=+numeros[i];    
 }
 
+promedio = sumatoria/numeros.length;
 document.getElementById("resultado").value = sumatoria;
+
+document.getElementById("promedio").value = promedio;
